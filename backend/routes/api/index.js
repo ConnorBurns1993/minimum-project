7f33f88709afb2d3b1b71b6962f218e801ade669
Connor Burns <cburns1993@gmail.com>
2022-06-17 15:41:07 -0700

    api index work

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -11,4 +11,27 @@ router.use("/session", sessionRouter);
 
 router.use("/users", usersRouter);
 
+router.get(
+  "/set-token-cookie",
+  asyncHandler(async (_req, res) => {
+    const user = await User.findOne({
+      where: {
+        name: "Demo-User",
+      },
+    });
+    setTokenCookie(res, user);
+    return res.json({ user });
+  })
+);
+
+const { restoreUser } = require("../../utils/auth.js");
+router.get("/restore-user", restoreUser, (req, res) => {
+  return res.json(req.user);
+});
+
+const { requireAuth } = require("../../utils/auth.js");
+router.get("/require-auth", requireAuth, (req, res) => {
+  return res.json(req.user);
+});
+
 module.exports = router;

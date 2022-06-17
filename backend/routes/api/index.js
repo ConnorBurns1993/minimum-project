c3b31af2abf89738160163dafc76cdfeaa167bac
Connor Burns <cburns1993@gmail.com>
2022-06-17 15:45:09 -0700

    api debugging

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -24,12 +24,10 @@ router.get(
   })
 );
 
-const { restoreUser } = require("../../utils/auth.js");
 router.get("/restore-user", restoreUser, (req, res) => {
   return res.json(req.user);
 });
 
-const { requireAuth } = require("../../utils/auth.js");
 router.get("/require-auth", requireAuth, (req, res) => {
   return res.json(req.user);
 });

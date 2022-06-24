8facc716b75ba2ea546bbd1f63e031eead8e696e
Connor Burns <cburns1993@gmail.com>
2022-06-24 11:09:56 -0700

    finished store for articles

---
+++ frontend/src/components/SingleArticle/index.js
@@ -0,0 +1,5 @@
+const SingleArticle = () => {
+  <h2>SingleArticle Component</h2>;
+};
+
+export default SingleArticle;
--- frontend/src/store/article.js
+++ frontend/src/store/article.js
@@ -1,6 +1,17 @@
 import { csrfFetch } from "./csrf";
+import { ValidationError } from "../utils/validationError";
 
+const ADD_ARTICLE = "articles/ADD_ARTICLE";
 const LOAD_ARTICLES = "articles/LOAD_ARTICLES";
+const UPDATE_ARTICLE = "articles/UPDATE_ARTICLE";
+const DELETE_ARTICLE = "articles/LOAD_ARTICLE";
+
+const add = (article) => {
+  return {
+    type: ADD_ARTICLE,
+    article,
+  };
+};
 
 const load = (articles) => {
   return {
@@ -9,14 +20,90 @@ const load = (articles) => {
   };
 };
 
+const update = (article) => {
+  return {
+    type: UPDATE_ARTICLE,
+    article,
+  };
+};
+
+const destroy = (articleId) => {
+  return {
+    type: DELETE_ARTICLE,
+    articleId,
+  };
+};
+
 export const loadArticles = () => async (dispatch) => {
   const response = await csrfFetch(`/api/articles`);
-  console.log("You are in load response");
 
   if (response.ok) {
-    console.log("Respone was ok!");
     const articles = await response.json();
     dispatch(load(articles));
+  }
+};
+
+export const loadOneArticle = (articleId) => async (dispatch) => {
+  const response = await csrfFetch(`/api/articles/${articleId}`);
+
+  if (response.ok) {
+    const articles = await response.json();
+    dispatch(add(articles));
+  }
+};
+
+export const addArticle = (newArticle) => async (dispatch) => {
+  try {
+    const response = await csrfFetch(`/api/articles/new`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newArticle),
+    });
+
+    if (!response.ok) {
+      let errors;
+      if (response.status === 422) {
+        errors = await response.json();
+        throw new ValidationError(errors.errors, response.statusText);
+      } else {
+        let errorsJSON;
+        errors = await response.text();
+        try {
+          errorsJSON = JSON.parse(errors);
+        } catch {
+          throw new Error(errors);
+        }
+        throw new Error(`${errorsJSON.title}: ${errorsJSON.message}`);
+      }
+    }
+    const article = await response.json();
+    dispatch(add(article));
+    return article;
+  } catch (errors) {
+    throw errors;
+  }
+};
+
+export const updateArticle = (article) => async (dispatch) => {
+  const response = await csrfFetch(`/api/articles/${article.id}`, {
+    method: "PUT",
+    body: JSON.stringify(article),
+  });
+
+  if (response.ok) {
+    const article = await response.json();
+    dispatch(update(article));
+  }
+};
+
+export const destroyArticle = (articleId) => async (dispatch) => {
+  const response = await csrfFetch(`/api/articles/${articleId}`, {
+    method: "DELETE",
+  });
+  if (response.ok) {
+    const { id } = await response.json();
+    dispatch(destroy(id));
+    return articleId;
   }
 };
 
@@ -25,12 +112,39 @@ const initialState = {};
 const articleReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_ARTICLES:
-      console.log("You are in the reducer");
       const normalArticles = {};
       action.articles.forEach((article) => {
         normalArticles[article.id] = article;
       });
       return normalArticles;
+
+    case ADD_ARTICLE:
+      if (!state[action.business.id]) {
+        const normalAddArticle = {
+          ...state,
+          [action.article.id]: action.article,
+        };
+        const articleList = normalAddArticle.list.map(
+          (id) => normalAddArticle[id]
+        );
+        articleList.push(action.article);
+        return normalAddArticle;
+      }
+
+      break;
+
+    case DELETE_ARTICLE:
+      const deletedArticleState = { ...state };
+      delete deletedArticleState[action.articleId];
+      return deletedArticleState;
+
+    case UPDATE_ARTICLE:
+      const updatedArticleState = {
+        ...state,
+        [action.article.id]: action.article,
+      };
+      return updatedArticleState;
+
     default:
       return state;
   }
